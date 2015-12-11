32a08871e7461715afec5f3e0f02ded1b95dae06
roy-cascadeo <roy.salvador@gmail.com>
2015-12-11 09:02:25 -0800

    Added Delete User functionality

---
+++ cgi-bin/delete-user.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+import cgi
+import cgitb; cgitb.enable()
+import MySQLdb as mdb
+from template import display
+from model.database import con
+from passlib.hash import sha512_crypt
+import os, time, sys, session, Cookie, json
+import utilities
+
+def main():
+	form = cgi.FieldStorage()
+	
+	#email = form.getvalue('email')
+	userprofile = form.getvalue('user')
+
+	#TODO: For fname, lname == None redirect to login page
+	#TODO: Implement sessions using Cookies
+	
+	try:
+		cur = con.cursor()
+
+		sess = session.Session(expires=365*24*60*60, cookie_path='/')
+		lastvisit = sess.data.get('lastvisit')
+		email= sess.data.get('user')
+		print sess.cookie
+
+		if email is None:
+			print "Location: login.py?redirect=1\r\n"
+
+		command = "SELECT * FROM Users WHERE Email = '" + email + "'";
+		cur.execute(command)
+		user= cur.fetchone()
+
+		command = "DELETE FROM Users Where Email = '" + userprofile +  "'";		
+		cur.execute(command)
+		con.commit()
+
+		command = "SELECT * from ComicBooks"	
+		cur.execute(command)
+		rows = cur.fetchall()
+		titles = []
+		for row in rows:
+			titles.append(row)
+		
+
+		sidebar = utilities.getSideBar(email, user[9], cur)
+		successMsg = "<strong>Success:</strong> User with email '" + userprofile + "' has been deleted."
+		print display("home.html").render(user=user,titles=titles,sidebar=sidebar,search=' ',genre=None,publisher=None, success=successMsg)
+
+	except mdb.Error, e:
+	    if con:
+	        con.rollback()
+
+	    command = "SELECT * FROM Users WHERE Email = '" + userprofile + "'";
+	    cur.execute(command)
+	    userprof = cur.fetchone() #
+
+	    command = "SELECT * from ComicBooks NATURAL JOIN UserCart WHERE Email='" + userprofile + "'"
+            cur.execute(command)
+            rows = cur.fetchall()
+	    titles = []
+	    for row in rows:
+		titles.append(row)
+
+	    command = "SELECT * from ComicBooks NATURAL JOIN UserOwned WHERE Email='" + userprofile + "'"
+	    cur.execute(command)
+	    rows = cur.fetchall()
+	    own = []
+	    for row in rows:
+		own.append(row)
+
+	    sidebar = utilities.getSideBar(email, user[9], cur)
+
+            #print display("home.html").render(user=user,sidebar=sidebar,error=e.args[1])
+	    if 'FOREIGN KEY' in e.args[1] :
+		errMsg = '<strong>Database Error:</strong> Foreign key constraint violated. Make sure to remove child records first.'
+	    else : 
+	    	errMsg = e.args[1]
+            print display("user-profile.html").render(user=user,userprof=userprof,sidebar=sidebar,titles=titles,own=own,error=errMsg)
+        sess.close()
+
+if __name__ == '__main__':
+	main()
